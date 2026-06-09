6f6aeca27bc10645e290fd89848050181c11a716
Sriram N V <sriramnvilvanathan@gmail.com>
2026-06-09 12:15:47 +0530

    fourth excercise

--- day1/TimeConv.py
+++ day1/TimeConv.py
@@ -2,14 +2,17 @@
 
 secs = int(input("Enter the seconds: ")) # 14423
 
-mins = secs // 60   #   mins = 240 min
-sec = secs % 60     #   sec = 23 14423%60 = 23
+mins = secs // 60   
 
-hrs = mins // 60    #   hrs = 4 hrs 240 / 60 = 4
-min = mins % 60     #   min = 0     240 % 60 = 0
+sec = secs % 60     
+
+hrs = mins // 60    
+
+min = mins % 60    
 
 day = hrs // 24
+
 hr = hrs %  24
 
 
-print(f"{day}:{hr}:{min}:{sec}")
+print(f"{day}:{hr}:{min}:{sec} in day:hour:min:sec")
