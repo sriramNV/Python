186a6a522383f0b68a1b4c6aca60efbd8b4937f3
Sriram N V <sriramnvilvanathan@gmail.com>
2026-06-09 11:29:05 +0530

    updating first exercise

--- day1/TempConv.py
+++ day1/TempConv.py
@@ -1,7 +1,15 @@
 #   The first exercise to convert the given temparature from celsius to farenheit
 
-temp = float(input("Enter the temparature: "))
-celsius = (temp - 32) * (5/9)
-farenheit = (temp * float(9 / 5)) + 32
-print(f"The {temp} degree celsius is {farenheit:.2f} degree farenheit")
-print(f"The {temp} degree farenheit is {celsius:.2f} degree celius")
+inp = input("Enter the temparature with c/f (eg. 15c or 111f): ")
+
+temp = int(inp[:-1])
+deg = inp[-1]
+
+if deg == 'c':
+    farenheit = (temp * float(9 / 5)) + 32
+    print(f"The {temp} degree celsius is {farenheit:.2f} degree farenheit")
+elif deg == 'f':
+    celsius = (temp - 32) * (5/9)
+    print(f"The {temp} degree farenheit is {celsius:.2f} degree celius")
+else:
+    print("wrong temp")
