ccd71d5658d2eedace0f3830194162fac1388d24
Sriram N V <sriramnvilvanathan@gmail.com>
2026-06-11 21:53:25 +0530

    elevnth exercise

---
+++ Loops/fibonacci.py
@@ -0,0 +1,13 @@
+# Fibonacci sequence generator upto given number(it generates upto 8 if given input is 10 since the next number in seq is 13 which is higher than 10)
+
+SUm = 0
+first = 1
+second = 1
+n = int (input("Enter N: "))
+print(first)
+print(second)
+while (first + second) <= n:
+    print(first + second)
+    t = second
+    second += first
+    first = t
